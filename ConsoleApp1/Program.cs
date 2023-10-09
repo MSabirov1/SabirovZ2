4267d4155b3144f8fbebc7c54166afabfe524c22
AzhimyratSharabidinov <sharabidinovmarvel@gmail.com>
2023-10-09 14:07:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using SabirovZ2;
+
+static int CheckInt()
+{
+    int a = 0;
+    while (true)
+    {
+        Console.Write("Write int: ");
+        string s = Console.ReadLine();
+        if (int.TryParse(s, out a))
+            break;
+    }
+    return a;
+}
+
+static void Main()
+{
+    try
+    {
+        Console.WriteLine("First:");
+        int n = CheckInt();
+        if (Second.O1(n))
+            Console.WriteLine("True");
+        else
+            Console.WriteLine("False");
+
+
+        Console.WriteLine("Second:");
+        Console.WriteLine("Write number 1:");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Write number 2:");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        if (n1 > n2)
+            Console.WriteLine($"{n1} > {n2}");
+        else if (n1 < n2)
+            Console.WriteLine($"{n1} < {n2}");
+        else
+            Console.WriteLine($"{n1} = {n2}");
+
+
+        Console.WriteLine("Third:");
+        int summ = 0;
+        int a;
+        do
+        {
+            a = CheckInt();
+            summ += a;
+        } while (a != 0);
+        Console.WriteLine($"Summ = {summ}");
+
+        Console.WriteLine("Fourth:");
+        Second.O4();
+
+
+        Console.WriteLine("Fifth:");
+        Second.O5();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+Main();
+
+
+
+
+
+
---
+++ ConsoleApp1/Second.cs
@@ -0,0 +1,129 @@
+﻿
+namespace SabirovZ2
+{
+    internal class Second
+    {
+        private static int CheckInt()
+        {
+            int a = 0;
+            while (true)
+            {
+                Console.Write("Write int: ");
+                string s = Console.ReadLine();
+                if (int.TryParse(s, out a))
+                    break;
+            }
+            return a;
+        }
+        public static bool O1(int n) => n % 3 == 0 && n % 7 == 0;
+
+        public static void O4()
+        {
+            int date = 0;
+            while (date < 1 || date > 7)
+            {
+                try
+                {
+                    date = CheckInt();
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            switch (date)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Monday");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Tuesday");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Wednesday");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Thursday");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Friday");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Saturday");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Sunday");
+                        break;
+                    }
+            }
+        }
+
+        public static void O5()
+        {
+            bool a = true;
+            while (a)
+            {
+                Console.Write("Write day: ");
+                string s = Console.ReadLine();
+                switch (s)
+                {
+                    case "Monday":
+                        {
+                            Console.WriteLine(1);
+                            a = false;
+                            break;
+                        }
+                    case "Tuesday":
+                        {
+                            Console.WriteLine(2);
+                            a = false;
+                            break;
+                        }
+                    case "Wendnesday":
+                        {
+                            Console.WriteLine(3);
+                            a = false;
+                            break;
+                        }
+                    case "Thursday":
+                        {
+                            Console.WriteLine(4);
+                            a = false;
+                            break;
+                        }
+                    case "Friday":
+                        {
+                            Console.WriteLine(5);
+                            a = false;
+                            break;
+                        }
+                    case "Saturday":
+                        {
+                            Console.WriteLine(6);
+                            a = false;
+                            break;
+                        }
+                    case "Sunday":
+                        {
+                            Console.WriteLine(7);
+                            a = false;
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
